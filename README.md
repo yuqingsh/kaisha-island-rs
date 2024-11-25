3ebce6267defa0c51ef7379ee635398f2d283432
yuqingsh <yuqingsh81@gmail.com>
2024-11-25 16:27:03 +0800

    all download feature good

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 This is a dummy project of mine (@yuqingsh) with Tongji University.
 
-I intended to get my hands dirty with some raw and real data before I start my phd project.
-The project's aim is to discover the relation between the extension of Shanghai City and the change of
-vegetation cover area through high-resolution satellite image.
+I intended to get my hands dirty with some raw and real data before I start my phd thesis.
+The project uses real Sentinel-2 RGB image and study the land cover classification.
 
 ## Requirement
 
@@ -16,11 +15,4 @@ torch==2.5.1
 
 ## Research Outline
 
-Since we are talking about the realationship between the extension of city and
-vegetation cover area, we need a time series of data. The time interval I chose
-is from Jan 1 2010 to Dec 31 2023. For each months, we download the
-
-## Data Download
-
-Now I have my AOI (aera of interest) decided, I need to download data for analysis. I chose Sentinel-2,
-since it doesn't require any application for usage and has mature API for automated workflow.
+The util.py file provides a automated download method from Sentinel Hub. For each month, a mosaicked image with least CC is downloaded.
--- utils.py
+++ utils.py
@@ -1,26 +1,17 @@
 import yaml
 import os
+import datetime
+from dateutil.relativedelta import relativedelta
 from sentinelhub.geo_utils import bbox_to_dimensions
 from sentinelhub import SHConfig, SentinelHubRequest, BBox, CRS, DataCollection, MimeType, MosaickingOrder
 import matplotlib.pyplot as plt
 
+# GLOBAL VARIABLES
+CONFIG_PATH = "config.yaml"
+RESOLUTION = 10
 KAISHA_ISLAND_BOUNDING_BOX = BBox(bbox=[120.556068, 32.003272, 120.692711, 32.078502], crs=CRS.WGS84)
-
-
-resolution =  10
-kaisha_bbox = BBox(bbox=KAISHA_ISLAND_BOUNDING_BOX, crs=CRS.WGS84)
-kaisha_size = bbox_to_dimensions(kaisha_bbox, resolution=resolution)
-
-with open("config.yaml", "r") as file:
-        config_data = yaml.safe_load(file)
-
-config = SHConfig()
-config.sh_client_id = config_data['sentinelhub']['client_id']
-config.sh_client_secret = config_data['sentinelhub']['client_secret']
-
-print(f"Image shape at {resolution} m resolution: {kaisha_size} pixels")
-
-evalscript_true_color = """
+KAISHA_SIZE = bbox_to_dimensions(KAISHA_ISLAND_BOUNDING_BOX, resolution=RESOLUTION)
+EVALSCRIPT_TRUE_COLOR = """
     //VERSION=3
 
     function setup() {
@@ -39,34 +30,89 @@ evalscript_true_color = """
     }
 """
 
-request_true_color = SentinelHubRequest(
-    evalscript=evalscript_true_color,
-    input_data=[
-        SentinelHubRequest.input_data(
-            data_collection=DataCollection.SENTINEL2_L1C,
-            time_interval=("2024-10-01", "2024-10-30"),
-            mosaicking_order=MosaickingOrder.LEAST_CC,
-        )
-    ],
-    responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
-    bbox=KAISHA_ISLAND_BOUNDING_BOX,
-    size=kaisha_size,
-    config=config,
-)
+def load_cfg_file(cfg_path):
+    """
+    Load the configuration file. The config file contains the Sentinel Hub client ID and secret.
+    :param cfg_path: path to the configuration file
+    :return: configuration object
+    """
+    with open(cfg_path, "r") as file:
+        config_data = yaml.safe_load(file)
 
-true_color_imgs = request_true_color.get_data()
-print(f"Returned data is of type = {type(true_color_imgs)} and length {len(true_color_imgs)}.")
-print(f"Single element in the list is of type {type(true_color_imgs[-1])} and has shape {true_color_imgs[-1].shape}")
+    config = SHConfig()
+    config.sh_client_id = config_data['sentinelhub']['client_id']
+    config.sh_client_secret = config_data['sentinelhub']['client_secret']
+    return config
 
-image = true_color_imgs[0]
-print(f"Image type: {image.dtype}")
+def save_image_to_disk(image, output_path):
+    """
+    Save the image to disk
+    :param image: image to save
+    :param output_path: path to save the image
+    """
+    plt.imsave(output_path, image)
+    print(f"Image saved to {output_path}")
 
-# Save the image to disk
-output_path = "rgb_image.png"
-plt.imsave(output_path, image)
-print(f"Image saved to {output_path}")
+def download_single_image(bbox, size, cfg, evalscript, time_interval=("2024-10-01", "2024-10-30")):
+    """
+    Download a single image from Sentinel Hub
+    :param bbox: bounding box of the AOI
+    :param resolution: resolution of the image in meters
+    :param evalscript: evalscript to use for the request
+    :param time_interval: time interval for the request
+    :return: image
+    """
 
-# Visualize the image
-plt.imshow(image)
-plt.axis('off')  # Hide axis
-plt.show()
+    request = SentinelHubRequest(
+        evalscript=evalscript,
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L1C,
+                time_interval=time_interval,
+                mosaicking_order=MosaickingOrder.LEAST_CC,
+            )
+        ],
+        responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
+        bbox=bbox,
+        size=size,
+        config=cfg,
+    )
+
+    image = request.get_data()[0]
+    return image
+
+def download_images_to_disk(bbox, size, cfg, evalscript, time_interval=("2020-1-01", "2023-12-31"), output_dir="data/"):
+    """
+    Download all images from Sentinel Hub in the given interval and save them to disk
+    For each month, a single image mosaicked by least CC is downloaded
+    :param bbox: bounding box of the AOI
+    :param resolution: resolution of the image in meters
+    :param evalscript: evalscript to use for the request
+    :param time_interval: time interval for the request
+    :param output_dir: directory to save the images
+    """
+
+    # Create the output directory if it does not exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    start_date = datetime.datetime.strptime(time_interval[0], "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(time_interval[1], "%Y-%m-%d")
+    current_date = start_date
+
+    while current_date <= end_date:
+        month_end = current_date + relativedelta(months=1) - datetime.timedelta(days=1)
+        if month_end > end_date:
+            month_end = end_date
+        current_month = (current_date.strftime("%Y-%m-%d"), month_end.strftime("%Y-%m-%d"))
+        save_image_to_disk(download_single_image(bbox, size, cfg, evalscript, time_interval=current_month), f"{output_dir}/{current_month[0]}.png")
+        current_date += relativedelta(months=1)
+
+    print("All images downloaded and saved to disk")
+
+def main(args=None):
+    cfg = load_cfg_file(CONFIG_PATH)
+    download_images_to_disk(KAISHA_ISLAND_BOUNDING_BOX, KAISHA_SIZE, cfg, EVALSCRIPT_TRUE_COLOR)
+
+if __name__=="__main__":
+    main()
